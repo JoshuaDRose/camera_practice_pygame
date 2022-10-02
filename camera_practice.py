45db5e75ff991b7d320e3c0aa873387ce63497d6
ShinJaehun <gunsmoke.shin@gmail.com>
2022-10-02 16:31:36 +0900

    mouse control

--- camera_practice.py
+++ camera_practice.py
@@ -1,3 +1,4 @@
+from signal import SIG_DFL
 import pygame, sys
 from random import randint
 
@@ -37,119 +38,165 @@ class Player(pygame.sprite.Sprite):
 		self.rect.center += self.direction * self.speed
 
 class CameraGroup(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self.display_surface = pygame.display.get_surface()
-        
-        # camera offset
-        self.offset = pygame.math.Vector2()
-        self.half_w = self.display_surface.get_size()[0] // 2
-        self.half_h = self.display_surface.get_size()[1] // 2
+	def __init__(self):
+		super().__init__()
+		self.display_surface = pygame.display.get_surface()
+		
+		# camera offset
+		self.offset = pygame.math.Vector2()
+		self.half_w = self.display_surface.get_size()[0] // 2
+		self.half_h = self.display_surface.get_size()[1] // 2
 
-        # box setup
-        self.camera_borders = {'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
-        l = self.camera_borders['left']
-        t = self.camera_borders['top']
-        w = self.display_surface.get_size()[0] - (self.camera_borders['left'] + self.camera_borders['right'])
-        h = self.display_surface.get_size()[1] - (self.camera_borders['top'] + self.camera_borders['bottom'])
-        self.camera_rect = pygame.Rect(l, t, w, h)
+		# box setup
+		self.camera_borders = {'left': 200, 'right': 200, 'top': 100, 'bottom': 100}
+		l = self.camera_borders['left']
+		t = self.camera_borders['top']
+		w = self.display_surface.get_size()[0] - (self.camera_borders['left'] + self.camera_borders['right'])
+		h = self.display_surface.get_size()[1] - (self.camera_borders['top'] + self.camera_borders['bottom'])
+		self.camera_rect = pygame.Rect(l, t, w, h)
 
-        # ground
-        self.ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
-        self.ground_rect = self.ground_surf.get_rect(topleft=(0,0))
-    
-        # camera speed
-        self.keyboard_speed = 5
+		# ground
+		self.ground_surf = pygame.image.load('graphics/ground.png').convert_alpha()
+		self.ground_rect = self.ground_surf.get_rect(topleft=(0,0))
+	
+		# camera speed
+		self.keyboard_speed = 5
+		self.mouse_speed = 0.4
 
-    def center_target_camera(self, target):
-        self.offset.x = target.rect.centerx - self.half_w
-        self.offset.y = target.rect.centery - self.half_h
+	def center_target_camera(self, target):
+		self.offset.x = target.rect.centerx - self.half_w
+		self.offset.y = target.rect.centery - self.half_h
 
-    def box_target_camera(self, target):
-        if target.rect.left < self.camera_rect.left:
-            self.camera_rect.left = target.rect.left
-        if target.rect.right > self.camera_rect.right:
-            self.camera_rect.right = target.rect.right
-        if target.rect.top < self.camera_rect.top:
-            self.camera_rect.top = target.rect.top
-        if target.rect.bottom > self.camera_rect.bottom:
-            self.camera_rect.bottom = target.rect.bottom
+	def box_target_camera(self, target):
+		if target.rect.left < self.camera_rect.left:
+			self.camera_rect.left = target.rect.left
+		if target.rect.right > self.camera_rect.right:
+			self.camera_rect.right = target.rect.right
+		if target.rect.top < self.camera_rect.top:
+			self.camera_rect.top = target.rect.top
+		if target.rect.bottom > self.camera_rect.bottom:
+			self.camera_rect.bottom = target.rect.bottom
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
-        self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
+		self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
+		self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
 
-    def keyboard_control(self):
-        keys = pygame.key.get_pressed()
-        # if keys[pygame.K_a]:
-        #     self.offset.x -= self.keyboard_speed
-        # if keys[pygame.K_d]:
-        #     self.offset.x += self.keyboard_speed
-        # if keys[pygame.K_w]:
-        #     self.offset.y -= self.keyboard_speed
-        # if keys[pygame.K_s]:
-        #     self.offset.y += self.keyboard_speed
+	def keyboard_control(self):
+		keys = pygame.key.get_pressed()
+		# if keys[pygame.K_a]:
+		#     self.offset.x -= self.keyboard_speed
+		# if keys[pygame.K_d]:
+		#     self.offset.x += self.keyboard_speed
+		# if keys[pygame.K_w]:
+		#     self.offset.y -= self.keyboard_speed
+		# if keys[pygame.K_s]:
+		#     self.offset.y += self.keyboard_speed
 
-        # keyboard control + box camera
-        if keys[pygame.K_a]:
-            self.camera_rect.x -= self.keyboard_speed
-        if keys[pygame.K_d]:
-            self.camera_rect.x += self.keyboard_speed
-        if keys[pygame.K_w]:
-            self.camera_rect.y -= self.keyboard_speed
-        if keys[pygame.K_s]:
-            self.camera_rect.y += self.keyboard_speed
+		# keyboard control + box camera
+		if keys[pygame.K_a]:
+			self.camera_rect.x -= self.keyboard_speed
+		if keys[pygame.K_d]:
+			self.camera_rect.x += self.keyboard_speed
+		if keys[pygame.K_w]:
+			self.camera_rect.y -= self.keyboard_speed
+		if keys[pygame.K_s]:
+			self.camera_rect.y += self.keyboard_speed
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
-        self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
+		self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
+		self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
 
-    def keyboard_control2(self, target):
-        # 이게 더 나은거 아냐! (이건 box camera를 keyboard_control에 붙여서 구현!)
-        keys = pygame.key.get_pressed()
+	def keyboard_control2(self, target):
+		# 이게 더 나은거 아냐! (이건 box camera를 keyboard_control에 붙여서 구현!)
+		keys = pygame.key.get_pressed()
 
-        # keyboard control + box camera
-        if keys[pygame.K_a]:
-            self.camera_rect.x -= self.keyboard_speed
-        if keys[pygame.K_d]:
-            self.camera_rect.x += self.keyboard_speed
-        if keys[pygame.K_w]:
-            self.camera_rect.y -= self.keyboard_speed
-        if keys[pygame.K_s]:
-            self.camera_rect.y += self.keyboard_speed
+		# keyboard control + box camera
+		if keys[pygame.K_a]:
+			self.camera_rect.x -= self.keyboard_speed
+		if keys[pygame.K_d]:
+			self.camera_rect.x += self.keyboard_speed
+		if keys[pygame.K_w]:
+			self.camera_rect.y -= self.keyboard_speed
+		if keys[pygame.K_s]:
+			self.camera_rect.y += self.keyboard_speed
 
-        if target.rect.left < self.camera_rect.left:
-            self.camera_rect.left = target.rect.left
-        if target.rect.right > self.camera_rect.right:
-            self.camera_rect.right = target.rect.right
-        if target.rect.top < self.camera_rect.top:
-            self.camera_rect.top = target.rect.top
-        if target.rect.bottom > self.camera_rect.bottom:
-            self.camera_rect.bottom = target.rect.bottom
+		if target.rect.left < self.camera_rect.left:
+			self.camera_rect.left = target.rect.left
+		if target.rect.right > self.camera_rect.right:
+			self.camera_rect.right = target.rect.right
+		if target.rect.top < self.camera_rect.top:
+			self.camera_rect.top = target.rect.top
+		if target.rect.bottom > self.camera_rect.bottom:
+			self.camera_rect.bottom = target.rect.bottom
 
-        self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
-        self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
+		self.offset.x = self.camera_rect.left - self.camera_borders['left'] 
+		self.offset.y = self.camera_rect.top - self.camera_borders['top'] 
 
-    def custom_draw(self, player):
-        # key camera
-        self.keyboard_control()
+	def mouse_control(self):
+		mouse = pygame.math.Vector2(pygame.mouse.get_pos())
+		mouse_offset_vector = pygame.math.Vector2()
 
-        # box camera
-        self.box_target_camera(player)
+		left_border = self.camera_borders['left']
+		top_border = self.camera_borders['top']
+		right_border = self.display_surface.get_size()[0] - self.camera_borders['right']
+		bottom_border = self.display_surface.get_size()[1] - self.camera_borders['bottom']
 
-        # ground
-        ground_offset = self.ground_rect.topleft - self.offset
-        self.display_surface.blit(self.ground_surf, ground_offset)
+		if top_border < mouse.y < bottom_border:
+			if mouse.x < left_border:
+				mouse_offset_vector.x = mouse.x - left_border
+				pygame.mouse.set_pos((left_border, mouse.y))
+			if mouse.x > right_border:
+				mouse_offset_vector.x = mouse.x - right_border
+				pygame.mouse.set_pos((right_border, mouse.y))
+		elif mouse.y < top_border:
+			if mouse.x < left_border:
+				mouse_offset_vector = mouse - pygame.math.Vector2(left_border, top_border)
+				pygame.mouse.set_pos((left_border, top_border))
+			if mouse.x > right_border:
+				mouse_offset_vector = mouse - pygame.math.Vector2(right_border, top_border)
+				pygame.mouse.set_pos((right_border, top_border))
+		elif mouse.y > bottom_border:
+			if mouse.x < left_border:
+				mouse_offset_vector = mouse - pygame.math.Vector2(left_border, bottom_border)
+				pygame.mouse.set_pos((left_border, bottom_border))
+			if mouse.x > right_border:
+				mouse_offset_vector = mouse - pygame.math.Vector2(right_border, bottom_border)
+				pygame.mouse.set_pos((right_border, bottom_border))
 
-        # active elements
-        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
 
-        # pygame.draw.rect(self.display_surface, 'yellow', self.camera_rect, 5)
+		if left_border < mouse.x < right_border:
+			if mouse.y < top_border:
+				mouse_offset_vector.y = mouse.y - top_border
+				pygame.mouse.set_pos((mouse.x, top_border))
+			if mouse.y > bottom_border:
+				mouse_offset_vector.y = mouse.y - bottom_border
+				pygame.mouse.set_pos((mouse.x, bottom_border))
+
+		self.offset += mouse_offset_vector * self.mouse_speed
+
+	def custom_draw(self, player):
+		# key camera
+		# self.keyboard_control()
+
+		# box camera
+		# self.box_target_camera(player)
+
+		# mouse control
+		self.mouse_control()
+
+		# ground
+		ground_offset = self.ground_rect.topleft - self.offset
+		self.display_surface.blit(self.ground_surf, ground_offset)
+
+		# active elements
+		for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
+			offset_pos = sprite.rect.topleft - self.offset
+			self.display_surface.blit(sprite.image, offset_pos)
+
+		# pygame.draw.rect(self.display_surface, 'yellow', self.camera_rect, 5)
 
 pygame.init()
 screen = pygame.display.set_mode((800,600))
 clock = pygame.time.Clock()
-
+pygame.event.set_grab(True)
 # setup 
 camera_group = CameraGroup()
 player = Player((640,360),camera_group)
@@ -164,6 +211,10 @@ while True:
 		if event.type == pygame.QUIT:
 			pygame.quit()
 			sys.exit()
+		if event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				pygame.quit()
+				sys.exit()
 
 	screen.fill('#71ddee')
 
